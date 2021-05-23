04b8f320f511891d4fbf84757bd4de41736c69fd
R4mm4 <65689530+R4mm4@users.noreply.github.com>
2021-05-22 22:08:12 -0500

    La aplicacion muestra una lista y puede pasar entre paginas de forma dinamica

---
+++ src/app/userslist/userslist.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+@Component({
+  selector: 'app-userslist',
+  templateUrl: './userslist.page.html',
+  styleUrls: ['./userslist.page.scss'],
+})
+export class UserslistPage implements OnInit {
+  characters=[]
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.http.get<any>('https://schoolido.lu/api/cards/')
+    .subscribe(res=>{
+      console.log(res);
+      this.characters=res.results;
+    }
+      )
+  }
+
+}
